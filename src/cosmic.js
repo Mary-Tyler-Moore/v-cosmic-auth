a46d1d8fc613d52810b88f6288f70d5923107738
Mary-Tyler-Moore <46167401+Mary-Tyler-Moore@users.noreply.github.com>
2019-05-31 22:47:02 -0400

    Update cosmic.js

--- src/cosmic.js
+++ src/cosmic.js
@@ -1,10 +1,10 @@
 const Cosmic = require('cosmicjs')();
 const database = Cosmic.bucket({
-  slug: 'cosmic-vueauth',
+  slug: '854007e0-8410-11e9-82a4-11047fb34b62',
   // write_key: process.env.WRITE_KEY,
   // read_key: process.env.READ_KEY
-  write_key: 'tJ4YxwhYgatffh4WAVEJiDZ9T7pRJPpMVOekTUuKROS2crlDRI',
-  read_key: 'xDF5EHQ5KgcWdsEc5PxCTLicSuSX839HJejXsg1jJ57ZF1ixVy'
+  write_key: '8aPqs3WIN81n22VIjY1GxVi2a35iaji7zdlx1QIpW9w1qWFdw5',
+  read_key: '6khkax9fW4qZlHXjovTXl6krh6nDdETJaGvhwbeaBKp0xCxeq0'
 })
 
 module.exports = database
